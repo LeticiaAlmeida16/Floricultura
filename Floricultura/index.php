7cffe3a79447bc057a883e9c09a147b92e13ee4b
raphaelaluvi <raphaluvi@gmail.com>
2024-11-07 12:04:14 -0300

    falta tudo

--- Floricultura/index.php
+++ Floricultura/index.php
@@ -2,25 +2,6 @@
     include 'C:\xampp\htdocs\Floricultura/banco_de_dados/conecta.php';
     include 'menu.php';
 ?>
-    <!-- Modal Search Start teste -->
-    <div class="modal fade" id="searchModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-        <div class="modal-dialog modal-fullscreen">
-            <div class="modal-content rounded-0">
-                <div class="modal-header">
-                    <h5 class="modal-title" id="exampleModalLabel">Search by keyword</h5>
-                    <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-                </div>
-                <div class="modal-body d-flex align-items-center">
-                    <div class="input-group w-75 mx-auto d-flex">
-                        <input type="search" class="form-control p-3" placeholder="keywords"
-                            aria-describedby="search-icon-1">
-                        <span id="search-icon-1" class="input-group-text p-3"><i class="fa fa-search"></i></span>
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-    <!-- Modal Search End -->
 
     <!-- Hero Start -->
     <div class="container-fluid py-5 mb-5 hero-header">
@@ -29,13 +10,6 @@
                 <div class="col-md-12 col-lg-7">
                     <h4 class="mb-3 text-secondary">Flores 100% Naturais</h4>
                     <h1 class="mb-5 display-3 text-primary">Flores & Arranjos Exclusivos</h1>
-                    <div class="position-relative mx-auto">
-                        <input class="form-control border-2 border-secondary w-75 py-3 px-4 rounded-pill" type="text"
-                            placeholder="Busque suas flores favoritas">
-                        <button type="submit"
-                            class="btn btn-primary border-2 border-secondary py-3 px-4 position-absolute rounded-pill text-white h-100"
-                            style="top: 0; right: 25%;">Buscar</button>
-                    </div>
                 </div>
                 <div class="col-md-12 col-lg-5">
                     <div id="carouselId" class="carousel slide position-relative" data-bs-ride="carousel">
@@ -171,7 +145,7 @@
                                      <!-- inicio card -->
 
                                     <?php
-                                        $sql = "SELECT * FROM estoque_flores LIMIT 10";
+                                        $sql = "SELECT * FROM estoque_flores LIMIT 8";
                                         $consulta = $conexao->query($sql); 
                                         while($dados = $consulta->fetch_assoc()){
 
@@ -199,23 +173,7 @@
                                         //var_dump($dados['imagem_flor']);
 
                                         
-                                    ?>
-                                    
-                                                
-                                    
-                                            
-                                            
-                                               
-                                                
-                                                    
-
-                                   
-                                    
-                                    
-                                    
-                                    
-                                    
-                                    
+                                    ?> 
                                 </div>
                             </div>
                         </div>
