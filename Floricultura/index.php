6f04c8fd41865626af4da6d28b623d59cbc4c23e
raphaelaluvi <raphaluvi@gmail.com>
2024-11-07 17:07:34 -0300

    footer.php

--- Floricultura/index.php
+++ Floricultura/index.php
@@ -587,65 +587,9 @@
     </div>
     <!-- Testimonial End -->
 
-    <!-- Footer Start -->
-    <div class="container-fluid bg-dark text-white-50 footer pt-5 mt-5">
-        <div class="container py-5">
-            <div class="pb-4 mb-4" style="border-bottom: 1px solid rgba(226, 175, 24, 0.5);">
-                <div class="row g-4">
-                    <div class="col-lg-3">
-                        <a href="#">
-                            <h1 class="text-primary mb-0">Luxe Blooms</h1>
-                            <p class="text-secondary mb-0">Flores Frescas para Todas as Ocasiões</p>
-                        </a>
-                    </div>
-                </div>
-            </div>
-            <div class="row g-5">
-                <div class="col-lg-3 col-md-6">
-                    <div class="footer-item">
-                        <h4 class="text-light mb-3">Por Que Escolher a Luxe Blooms?</h4>
-                        <p class="mb-4">Oferecemos flores frescas e arranjos personalizados que iluminam qualquer
-                            evento. Venha conhecer!</p>
-                        <a href="#" class="btn border-secondary py-2 px-4 rounded-pill text-primary">Saiba Mais</a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="d-flex flex-column text-start footer-item">
-                        <h4 class="text-light mb-3">Informações da Loja</h4>
-                        <a class="btn-link" href="#">Sobre Nós</a>
-                        <a class="btn-link" href="#">Contate-Nos</a>
-                        <a class="btn-link" href="#">Política de Privacidade</a>
-                        <a class="btn-link" href="#">Termos e Condições</a>
-                        <a class="btn-link" href="#">Política de Devolução</a>
-                        <a class="btn-link" href="#">FAQs & Ajuda</a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="d-flex flex-column text-start footer-item">
-                        <h4 class="text-light mb-3">Minha Conta</h4>
-                        <a class="btn-link" href="#">Minha Conta</a>
-                        <a class="btn-link" href="#">Detalhes da Compra</a>
-                        <a class="btn-link" href="#">Carrinho de Compras</a>
-                        <a class="btn-link" href="#">Lista de Desejos</a>
-                        <a class="btn-link" href="#">Histórico de Pedidos</a>
-                        <a class="btn-link" href="#">Pedidos Internacionais</a>
-                    </div>
-                </div>
-                <div class="col-lg-3 col-md-6">
-                    <div class="footer-item">
-                        <h4 class="text-light mb-3">Contato</h4>
-                        <p>Endereço: Rua das Orquídeas, 123 - Jardim Botânico, São Paulo</p>
-                        <p>Email: contato@luxeblooms.com</p>
-                        <p>Telefone: +0123 4567 8910</p>
-                        <p>Formas de Pagamento Aceitas</p>
-                        <img src="img/payment.png" class="img-fluid" alt="Métodos de pagamento">
-                    </div>
-                </div>
-            </div>
-        </div>
-    </div>
-
-    <!-- Footer End -->
+    <?php
+        include 'footer.php';
+    ?>
 
     <!-- Copyright Start -->
     <div class="container-fluid copyright bg-dark py-4">
