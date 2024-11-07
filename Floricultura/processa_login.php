da7890f705c246f63f61be940077355ca26562ca
LucasBardedlla <lucasss.bardella.f@gmail.com>
2024-11-07 20:20:29 -0300

    alterações back-end

--- Floricultura/processa_login.php
+++ Floricultura/processa_login.php
@@ -1,37 +1,47 @@
 <?php
 
-    //cria a sessao 
-    //session_start();
+    // Inicia a sessão para armazenar dados temporários do usuário
+    session_start();
 
-    //puxa a coneccao
+    // Importa o arquivo de conexão com o banco de dados
     require('../banco_de_dados/conecta.php');
 
-    //vem do login.html
+    // Recebe os dados enviados pelo formulário de login
     $email = $_POST['email'];
     $senha = $_POST['senha'];
 
+    // Consulta SQL para verificar se o usuário é um vendedor
+    $consulta_vendedor = "SELECT * FROM cadastro_vendedor WHERE email_vendedor = '$email' AND senha_vendedor = '$senha'";
+    $resultado_vendedor = $conexao->query($consulta_vendedor);
+    $isVendedor = $resultado_vendedor->num_rows === 1; // Retorna verdadeiro se o usuário é um vendedor
 
-    $consulta = "SELECT * FROM cadastro_vendedor WHERE email_vendedor = '$email' AND senha_vendedor = '$senha'";
+    // Consulta SQL para verificar se o usuário é um cliente (caso não seja um vendedor)
+    $consulta_cliente = "SELECT * FROM cadastro_cliente WHERE email_cliente = '$email' AND senha_cliente = '$senha'";
+    $resultado_cliente = $conexao->query($consulta_cliente);
+    $isCliente = $resultado_cliente->num_rows === 1; // Retorna verdadeiro se o usuário é um cliente
 
-    $resultado = $conexao->query($consulta);
-    //pega a quantidade de linhas entro da $resultado e atribui a uma variavel
-    $registros = $resultado->num_rows;
-  
-    //pegou o objeto resultado e fez uma associacao de tudo q achou
-    $resultado_usuario = mysqli_fetch_assoc($resultado);
+    if ($isVendedor) {
+        // Usuário é um vendedor
+        $dados_vendedor = mysqli_fetch_assoc($resultado_vendedor);
+        $_SESSION['id'] = $dados_vendedor['id'];
+        $_SESSION['nome'] = $dados_vendedor['nome'];
+        $_SESSION['email'] = $dados_vendedor['email'];
+        $_SESSION['tipo'] = 'vendedor';
 
-    //esse detalha a variavel inteira, mostra valores, tipo; usa pra lista, obejto
-    //var_dump($resultado_usuario);
+        // Redireciona para o painel do vendedor
+        header('Location: vendedorTeste.php');
+    } elseif ($isCliente) {
+        // Usuário é um cliente
+        $dados_cliente = mysqli_fetch_assoc($resultado_cliente);
+        $_SESSION['id'] = $dados_cliente['id'];
+        $_SESSION['nome'] = $dados_cliente['nome'];
+        $_SESSION['email'] = $dados_cliente['email'];
+        $_SESSION['tipo'] = 'cliente';
 
-    if($registros == 1){
-        //chama a variavel do banco de dados
-        $_SESSION['id'] = $resultado_usuario['id'];
-        $_SESSION['nome'] = $resultado_usuario['nome'];
-        $_SESSION['email'] = $resultado_usuario['email'];
-
+        // Redireciona para a página inicial do cliente
         header('Location: index.php');
-    }
-    else{
+    } else {
+        // Login falhou, redireciona para a página de login novamente
         header('Location: Login_v1/index.html');
     }
 ?>
